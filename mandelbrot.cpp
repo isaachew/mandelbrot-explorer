f131337ba523c3ba71727a73f7590564f65692ca
Isaac Hew <isaachewks@gmail.com>
2022-01-15 08:28:42 +0800

    Added basic mandelbrot function

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -5,14 +5,24 @@ compile with:
 emcc mandelbrot.cpp --bind -ffast-math -o mandelbrot.js
  */
 
+int maxiters=1000;
 
-
-
+int numits(std::complex<double> c){
+    std::complex<double> z=0;
+    for(int i=0;i<maxiters;i++){
+        if(norm(z)>4)return i;
+        z*=z;
+        z+=c;
+    }
+    return -1;
+}
 EMSCRIPTEN_BINDINGS(cmplx){
     emscripten::class_<std::complex<double>> cmplx("complex");//will be complex in JavaScript
     cmplx.constructor<>();
     cmplx.constructor<double,double>();
     cmplx.property("real",emscripten::select_overload<double()const>(&std::complex<double>::real));
     cmplx.property("imag",emscripten::select_overload<double()const>(&std::complex<double>::imag));
-    
+}
+EMSCRIPTEN_BINDINGS(mandelbrot){
+    emscripten::function("numits",&numits);
 }
