88467cdab27efaa3190cb1fd7e29a7831339fa4d
Isaac Hew <isaachewks@gmail.com>
2022-02-04 17:51:44 +0800

    Colour stops can be dragged past each other properly

--- palette.js
+++ palette.js
@@ -27,7 +27,7 @@ function updateGradient(){
 
     document.getElementById("paletteLength").textContent=palette.length
     for(var ind=0;ind<palette.stops.length;ind++){
-        var colStop=palette.stops[ind]
+        let colStop=palette.stops[ind]
         let csscol="#"+colStop.colour.map(a=>(a|0).toString(16).padStart(2,0)).join``
         grad.addColorStop(colStop.position,csscol)
 
@@ -40,38 +40,50 @@ let lastUpdate=performance.now()
 function updateHandles(){
     document.getElementById("colStops").innerHTML=""
     for(var ind=0;ind<palette.stops.length;ind++){
+        let curIndex=ind
         let colStop=palette.stops[ind]
         let dvel=document.createElement("div")
         dvel.classList.add("colstop")
+        dvel.id="paletteStop"+ind
         dvel.style.left=colStop.position*700+"px"
         let csscol="#"+colStop.colour.map(a=>(a|0).toString(16).padStart(2,0)).join``
         dvel.style.backgroundColor=csscol
         dvel.textContent="\u00a0"
         dvel.setAttribute("a",ind)
         dvel.draggable=true
-        dvel.addEventListener("dragstart",e=>{})
-        dvel.addEventListener("drag",e=>{
-            if(e.screenX==e.clientX&&e.clientX==0)return
-            let offset=e.clientX-document.getElementById("colStops").offsetLeft
-            if(offset<0)offset=0
-            if(offset>700)offset=700
-            palette.stops[dvel.getAttribute("a")].position=offset/700
-            dvel.style.left=offset+"px"
-
-            palette.stops.sort((a,b)=>a.position-b.position)
-            updateGradient()
-            let updTime=performance.now()
-            if(updTime-lastUpdate>100){
-                console.log(updTime)
-                render()
-                lastUpdate=updTime
-            }
-            //updateHandles()
+        dvel.addEventListener("dragstart",e=>{
+            selectedStop=curIndex
         })
         document.getElementById("colStops").append(dvel)
     }
 }
 
+document.getElementById("paletteDisplay").addEventListener("dragover",e=>{
+    e.preventDefault()
+    console.log(e.dataTransfer.getData("text/plain"),e.target)
+    if(e.screenX==e.clientX&&e.clientX==0)return
+    let offset=e.clientX-document.getElementById("colStops").offsetLeft
+    if(offset<0)offset=0
+    if(offset>700)offset=700
+
+    let newPos=palette.stops[selectedStop].position=offset/700
+    document.getElementById("paletteStop"+selectedStop).style.left=offset+"px"
+
+
+    let newIndex=palette.stops.findIndex(a=>a.position>newPos)
+    if(newIndex>selectedStop)newIndex--
+    selectedStop=newIndex
+    palette.stops.sort((a,b)=>a.position-b.position)
+    updateGradient()
+    updateHandles()
+
+    let updTime=performance.now()
+    if(updTime-lastUpdate>50){
+        lastUpdate=updTime
+        render()
+    }
+    //updateHandles()
+})
 
 document.addEventListener("dragover",function(e){
     e.preventDefault()
